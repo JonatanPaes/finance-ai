0eaa3c56a802b56df4feff3b6d144e76170fe312
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-11-15 22:08:04 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,18 @@
+import { SignInButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import { LogInIcon } from 'lucide-react'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 import { Button } from '../_components/ui/button'
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { userId } = await auth()
+
+  if (userId) {
+    redirect('/')
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -23,10 +32,12 @@ export default function LoginPage() {
           facilitando o controle do seu orçamento.
         </p>
 
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       <div className="relative h-full w-full">
