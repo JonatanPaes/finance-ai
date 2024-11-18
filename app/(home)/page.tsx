6de089723b32282e7598cd8253d47fd0f004a0d1
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-11-17 22:10:24 -0300

    feat: add last transaction component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation'
 import { Navbar } from '../_components/navbar'
 import { getDashboard } from '../_data/get-dashboard'
 import { ExpensesPerCategory } from './_components/expenses-per-category'
+import { LastTransactions } from './_components/last-transactions'
 import { SummaryCards } from './_components/summary-cards'
 import { TransactionsPieChart } from './_components/transactions-pie-chart'
 
@@ -47,6 +48,8 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
               />
             </div>
           </div>
+
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
