c74dff9844eae686a3a46ce457ce62de5f2f2fe2
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-11-17 22:19:01 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ import { getDashboard } from '../_data/get-dashboard'
 import { ExpensesPerCategory } from './_components/expenses-per-category'
 import { LastTransactions } from './_components/last-transactions'
 import { SummaryCards } from './_components/summary-cards'
+import TimeSelect from './_components/time-select'
 import { TransactionsPieChart } from './_components/transactions-pie-chart'
 
 interface HomeProps {
@@ -35,6 +36,9 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
+          <div className="flex items-center gap-3">
+            <TimeSelect />
+          </div>
         </div>
         <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
